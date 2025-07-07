e6da59663a8e2d32af526bbe820283640aed1a66
Jose Rios <jrbernabe@gmail.com>
2025-07-07 09:31:32 -0400

    server api

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 const {$hello, $consoleIt} = useNuxtApp()
 console.log('useNuxtApp', useNuxtApp())
+
+// API Server
+const { data } = await useFetch('/api/hello')
 </script>
 
 <template>
   <h1>Home Page</h1>
   <p>{{ $hello('Jose') }}</p>
   <p>{{ $consoleIt('Jose') }}</p>
+  <br><br>
+  <h2>Testing API</h2>
+  <p>{{ data }}</p>
 </template>
