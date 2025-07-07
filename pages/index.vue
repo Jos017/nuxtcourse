4d04e46096fffe7ad04e522031569fa8689e7652
Jose Rios <jrbernabe@gmail.com>
2025-07-07 09:27:27 -0400

    plugins

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+const {$hello, $consoleIt} = useNuxtApp()
+console.log('useNuxtApp', useNuxtApp())
+</script>
+
 <template>
   <h1>Home Page</h1>
+  <p>{{ $hello('Jose') }}</p>
+  <p>{{ $consoleIt('Jose') }}</p>
 </template>
